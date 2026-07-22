c4c22fe618a9438e024a98e8022f2e5bd310c31c
lisandro_rafaelano <lisandro@nursa.com>
2026-07-21 18:20:57 -0600

    update

--- aws/demo-aws-lambda/functions/nodejs-app/index.js
+++ aws/demo-aws-lambda/functions/nodejs-app/index.js
@@ -4,7 +4,7 @@ const serverless = require('serverless-http');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express version 4! version 2026');
+  res.send('Hello Express version 1! version 2026');
 });
 
 exports.handler = serverless(app);
