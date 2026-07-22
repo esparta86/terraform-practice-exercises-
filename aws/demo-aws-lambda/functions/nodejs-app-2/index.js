677b18628b84eaa95bd34b7a3bae4f584c682d1a
lisandro_rafaelano <lisandro@nursa.com>
2026-07-22 10:54:03 -0600

    testing to identify chnages

--- aws/demo-aws-lambda/functions/nodejs-app-2/index.js
+++ aws/demo-aws-lambda/functions/nodejs-app-2/index.js
@@ -4,7 +4,7 @@ const serverless = require('serverless-http');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express nodejs app 2 ! version 2026');
+  res.send('Hello Express nodejs app 5 ! version 2026');
 });
 
 exports.handler = serverless(app);
